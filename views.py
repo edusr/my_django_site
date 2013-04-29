72dfec86154e5e8beb43172bee08e924df0a7462
edusr <edusr@edusr-laptop.(none)>
2013-04-29 13:29:48 -0300

    added hours_ahead template

--- views.py
+++ views.py
@@ -17,5 +17,4 @@ def hours_ahead(request,offset):
 	except Exception, e:
 		raise Http404()
 	dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+	return render (request,'hours_ahead.html',{'hour_offset':offset,'next_time':dt})
